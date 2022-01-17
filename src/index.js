c2b69699a74064a97753c850d40e0b7d16b1c9f8
Tom <tomophilia@mac.com>
2022-01-17 09:44:53 -0500

    Finished tutorial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BudgetsProvider } from "./contexts/BudgetsContext"
+import { BudgetsProvider } from './contexts/BudgetsContext'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BudgetsProvider>
       <App />
     </BudgetsProvider>
+    
   </React.StrictMode>,
   document.getElementById("root")
 )
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import App from "./App"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import { BudgetsProvider } from "./contexts/BudgetsContext"
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BudgetsProvider>
+//       <App />
+//     </BudgetsProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// )
